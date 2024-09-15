8df87296e78e9467a12becea033d02eb3cadd8d8
nagata <hgtm.kabocha@gmail.com>
2024-09-16 07:44:21 +0900

    add DB

---
+++ _main.js
@@ -0,0 +1,40 @@
+(() => {
+
+  const contents = document.getElementById('contents');
+
+  //
+  const infiniteScrollObserver = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if ( ! entry.isIntersecting ) return;
+
+          infiniteScrollObserver.unobserve(entry.target);
+          loadContent();
+
+      });
+  });
+
+  //
+  let i = 0;
+  const max = 30;
+
+  const loadContent = async () => {
+
+      const response = await fetchDummy('https://example.com/load?i=' + i);
+
+      contents.insertAdjacentHTML('beforeend',
+              '<div>' +
+              '#' + (i + 1) + '<br>' +
+              await response.text() +
+              '</div>');
+
+      //
+      i++;
+
+      if ( i < max ) infiniteScrollObserver.observe(contents.lastElementChild);
+
+  };
+
+  //
+  loadContent();
+
+})();
